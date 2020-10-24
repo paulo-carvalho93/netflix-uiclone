da723b3aed79072b4dc870a929169e1984ff2690
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-10-24 13:38:53 -0300

    feat(netflix): adding css styles MovieRow component

--- src/components/MovieRow/MovieRow.js
+++ src/components/MovieRow/MovieRow.js
@@ -4,12 +4,16 @@ import './movierow.css';
 
 function MovieRow ({ title, items }) {
   return (
-    <div>
+    <div className="movieRow">
       <h2>{title}</h2>
-      <div className="movieRow--listarea">
-        {items.results.length > 0 && items.results.map((item, index) => (
-          <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
-        ))}
+      <div className="movieRow-listarea">
+        <div className="movieRow-list">
+          {items.results.length > 0 && items.results.map((item, index) => (
+            <div key={index} className="movieRow-item">
+              <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+            </div>
+            ))}
+        </div>
       </div>
     </div>
   );
